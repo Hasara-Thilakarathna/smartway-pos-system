8ae39c863cec7488c8c0ae96a03d13623b19bef5
Hasara-Thilakarathna <hasarathilakarathnaa4@gmail.com>
2023-10-21 15:28:33 +0530

    Create access to customer database

--- smartway-pos-system/src/main/java/lk/ijse/dep11/app/controller/ManageCustomerForm.java
+++ smartway-pos-system/src/main/java/lk/ijse/dep11/app/controller/ManageCustomerForm.java
@@ -2,23 +2,29 @@ package lk.ijse.dep11.app.controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.animation.ScaleTransition;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import lk.ijse.dep11.app.tm.Customer;
 
 import java.io.IOException;
 import java.net.URL;
 
 public class ManageCustomerForm {
     public AnchorPane root;
-    public TableView tblCustomers;
+    public TableView<Customer> tblCustomers;
     public ImageView imgBack;
     public JFXButton btnNewCustomer;
     public JFXTextField txtCustomerId;
@@ -36,12 +42,47 @@ public class ManageCustomerForm {
         Platform.runLater(primaryStage::sizeToScene);
     }
 
+    public void initialize() {
+        txtCustomerId.setEditable(false);
+        btnDelete.setDisable(true);
+        btnNewCustomer.fire();
+
+        int i=0;
+        for (String col : new String[]{"id", "name", "address"}) {
+            tblCustomers.getColumns().get(i).setCellValueFactory(new PropertyValueFactory<>(col));
+            i++;
+        }
+
+    }
+
     public void btnNewCustomerOnAction(ActionEvent actionEvent) {
+        for (TextField textField : new TextField[]{txtCustomerName, txtCustomerId, txtCustomerAddress}) {
+            textField.clear();
+        }
+        txtCustomerName.requestFocus();
+
     }
 
     public void btnSaveOnAction(ActionEvent actionEvent) {
+
     }
 
     public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void playAnimationOnMouseEnter(MouseEvent event) {
+            ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(100),(ImageView)event.getSource());
+            scaleTransition.setToX(1.3);
+            scaleTransition.setToY(1.3);
+            scaleTransition.play();
+
+    }
+
+    public void playAnimationOnMouseExit(MouseEvent event) {
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(100),(ImageView)event.getSource());
+        scaleTransition.setToX(1);
+        scaleTransition.setToY(1);
+        scaleTransition.play();
+
     }
 }
